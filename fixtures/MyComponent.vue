aafcc2226477894d7b8b3c812201606e2ad50fca
cole <bernankeic@gmail.com>
2023-06-14 17:07:22 +0800

    test: add fixtures

---
+++ fixtures/MyComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="my-class"></div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+.my-class {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
